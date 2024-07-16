27fc48cff4f46115c967a5b85962144d72d0c881
amarenderreddy <amarendereddyjakka@gmail.com>
2024-07-16 13:40:17 -0700

    Modify Provider package to net.discdd

--- legacy/common/src/main/java/com/fsck/k9/backends/DddBackendFactory.kt
+++ legacy/common/src/main/java/com/fsck/k9/backends/DddBackendFactory.kt
@@ -1,15 +1,10 @@
 package com.fsck.k9.backends
 
-import android.content.ContentResolver
 import android.content.Context
 import com.fsck.k9.Account
 import com.fsck.k9.backend.BackendFactory
 import com.fsck.k9.backend.api.Backend
 import com.fsck.k9.backend.ddd.DddBackend
-import com.fsck.k9.mail.oauth.OAuth2TokenProvider
-import com.fsck.k9.mail.ssl.TrustedSocketFactory
-import com.fsck.k9.mail.store.pop3.Pop3Store
-import com.fsck.k9.mail.transport.smtp.SmtpTransport
 import com.fsck.k9.mailstore.K9BackendStorageFactory
 
 class DddBackendFactory(
