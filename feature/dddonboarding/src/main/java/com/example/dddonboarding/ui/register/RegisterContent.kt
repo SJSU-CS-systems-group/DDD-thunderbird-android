db55a895428f45950dec6ff2f83f71f52ac4a015
ManasC478 <manas.chougule@gmail.com>
2024-12-03 12:59:07 -0800

    fix: update ui

--- feature/dddonboarding/src/main/java/com/example/dddonboarding/ui/register/RegisterContent.kt
+++ feature/dddonboarding/src/main/java/com/example/dddonboarding/ui/register/RegisterContent.kt
@@ -1,11 +1,19 @@
 package com.example.dddonboarding.ui.register
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import app.k9mail.core.ui.compose.designsystem.atom.Surface
 import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonText
 import app.k9mail.core.ui.compose.designsystem.atom.text.TextDisplayMedium
+import app.k9mail.core.ui.compose.designsystem.atom.textfield.TextFieldOutlined
+import app.k9mail.core.ui.compose.designsystem.molecule.input.PasswordInput
+import app.k9mail.core.ui.compose.designsystem.template.LazyColumnWithHeaderFooter
 import app.k9mail.core.ui.compose.designsystem.template.ResponsiveContent
+import app.k9mail.core.ui.compose.theme2.MainTheme
 
 @Composable
 internal fun RegisterContent(
@@ -16,10 +24,48 @@ internal fun RegisterContent(
         modifier = modifier
     ){
         ResponsiveContent {
-            TextDisplayMedium(
-                text = "Register Screen",
-            )
-            ButtonText(text = "Go login", onClick = onLoginClick)
+            LazyColumnWithHeaderFooter(
+                modifier = Modifier.fillMaxSize(),
+                header = {
+                    TextDisplayMedium(
+                        text = "Register Screen",
+                    )
+                },
+                footer = {
+                    RegisterFooter(
+                        modifer = Modifier
+                            .fillMaxWidth()
+                            .padding(top = MainTheme.spacings.quadruple),
+                        onLoginClick = onLoginClick
+                    )
+                },
+                verticalArrangement =  Arrangement.SpaceEvenly
+            ){
+                item {
+                    RegisterInputs()
+                }
+            }
         }
     }
+}
+
+@Composable
+private fun RegisterFooter(
+    onLoginClick: () -> Unit,
+    modifer: Modifier = Modifier
+) {
+    ButtonText(text = "Go login", onClick = onLoginClick)
+}
+
+@Composable
+private fun RegisterInputs(
+    modifier: Modifier = Modifier
+) {
+    TextFieldOutlined(value = "Prefix 1", onValueChange = {})
+    TextFieldOutlined(value = "Prefix 2", onValueChange = {})
+    TextFieldOutlined(value = "Prefix 3", onValueChange = {})
+    TextFieldOutlined(value = "Suffix 1", onValueChange = {})
+    TextFieldOutlined(value = "Suffix 2", onValueChange = {})
+    TextFieldOutlined(value = "Suffix 3", onValueChange = {})
+    PasswordInput(password = "Secret", onPasswordChange = {})
 }
