36474ddf5ed91ec825093012d5b85fbcf08f37d8
ManasC478 <manas.chougule@gmail.com>
2025-02-13 14:54:23 -0800

    update gradle

--- feature/dddonboarding/build.gradle.kts
+++ feature/dddonboarding/build.gradle.kts
@@ -13,4 +13,6 @@ dependencies {
     implementation("androidx.compose.material:material:1.7.7")
     implementation(projects.core.common)
     implementation(projects.feature.account.common)
+    implementation(project(":mail:common"))
+    implementation(project(":feature:account:setup"))
 }
