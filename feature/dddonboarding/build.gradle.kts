975339fb6e5bbd41f8202e98a7c5c192b2dfe3de
ben reed <ben.reed@sjsu.edu>
2025-06-22 17:48:15 -0700

    x

--- feature/dddonboarding/build.gradle.kts
+++ feature/dddonboarding/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(projects.core.ui.compose.common)
     implementation(projects.core.ui.compose.designsystem)
     implementation("androidx.compose.material:material:1.7.7")
+    implementation(projects.core.common)
     implementation(projects.feature.account.common)
     implementation(project(":mail:common"))
     implementation(project(":feature:account:setup"))
