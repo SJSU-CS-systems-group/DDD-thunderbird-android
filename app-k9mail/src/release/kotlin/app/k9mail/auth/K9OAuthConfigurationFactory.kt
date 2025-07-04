c28af6392b7746cf9aa7d8951c577bcea1548345
ben reed <ben.reed@sjsu.edu>
2025-07-03 22:18:51 -0700

    spotless fix

--- app-k9mail/src/release/kotlin/app/k9mail/auth/K9OAuthConfigurationFactory.kt
+++ app-k9mail/src/release/kotlin/app/k9mail/auth/K9OAuthConfigurationFactory.kt
@@ -6,7 +6,6 @@ import app.k9mail.core.common.oauth.OAuthConfigurationFactory
 @Suppress("ktlint:standard:max-line-length")
 class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
     override fun createConfigurations(): Map<List<String>, OAuthConfiguration> {
-        return mapOf(
-        )
+        return mapOf()
     }
 }
