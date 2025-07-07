2a1dd38e460baa7f2bc440a263253d6a234fd796
ben reed <bcr33d@gmail.com>
2025-07-07 12:20:33 -0700

    x

--- legacy/common/src/main/java/com/fsck/k9/backends/DddBackendFactory.kt
+++ legacy/common/src/main/java/com/fsck/k9/backends/DddBackendFactory.kt
@@ -9,14 +9,14 @@ import com.fsck.k9.backend.ddd.DddBackend
 import com.fsck.k9.mailstore.K9BackendStorageFactory
 
 class DddBackendFactory(
-    val context: Context,
+    override val context: Context,
     private val backendStorageFactory: K9BackendStorageFactory,
     private val preferences: Preferences,
 ) : BackendFactory, DddBackend.DDDFactory {
     override fun createBackend(account: Account): Backend {
         val backendStorage = backendStorageFactory.createBackendStorage(account)
         val uuid = account.uuid
-        return DddBackend(this, context, uuid, backendStorage)
+        return DddBackend(this, uuid, backendStorage)
     }
 
     override fun changeEmailAddress(uuid: String, email: String) {
