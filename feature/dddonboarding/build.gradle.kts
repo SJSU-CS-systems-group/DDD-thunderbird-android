c383aa87a918cf5538f21bb470b38c6c36f3809b
Benjamin Reed <bcr33d@gmail.com>
2025-07-06 19:32:22 -0700

    update DDD k9-common to 0.1 (#27)

--- feature/dddonboarding/build.gradle.kts
+++ feature/dddonboarding/build.gradle.kts
@@ -16,6 +16,6 @@ dependencies {
     implementation(project(":mail:common"))
     implementation(project(":feature:account:setup"))
     implementation(libs.client.adapter)
-    implementation("net.discdd.app:k9-common:0.0.1-SNAPSHOT")
+    implementation("net.discdd.app:k9-common:0.1")
     implementation(libs.androidx.material3.android)
 }
